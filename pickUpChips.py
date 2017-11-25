e1d615f5df0ce670e7e1aa5b0d4b4204daedeb44
KARMIT\Karmit <karmit.dandona@gmail.com>
2017-11-25 02:09:19 -0800

    Restructured code to flow from top to bottom in a more sequential order. The setup of the board has been implemented, and the core game loop is roughly programmed. The win condition and edge cases for the final turn have not been written yet. There has been NO testing so far.

--- pickUpChips.py
+++ pickUpChips.py
@@ -11,6 +11,78 @@ from __future__ import print_function
 from random import randint
 
 
+# --------------- Main handler ------------------
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
+
+    if (event['session']['application']['applicationId'] !=
+             "amzn1.echo-sdk-ams.app.26e18db1-50b1-46e9-86d1-eb9df427e0bf"):
+        raise ValueError("Invalid Application ID")
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
+
+
+# --------------- Events ------------------
+
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # Dispatch to your skill's launch
+    return get_welcome_response()
+
+
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill """
+
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to your skill's intent handlers
+    if intent_name == "StartGame":
+        return GameSetup(intent, session)
+    elif intent_name == "GetNumber":
+        if intent["slots"]["Number"]["value"] != 1 and intent["slots"]["Number"]["value"] != 2 and intent["slots"]["Number"]["value"] != 3:
+            session_attributes = session["sessionAttributes"]
+            speech_output = "Sorry, that sounded like an invalid number of chips. You can only take 1, 2, or 3 chips. How many chips do you take?"
+            reprompt_text = "Sorry, that sounded like an invalid number of chips. How many chips do you take from the table?"
+            return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
+        return GameLoop(intent, session)
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    else:
+        raise ValueError("Invalid intent")
+
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # add cleanup logic here
+
+
 # --------------- Helpers that build all of the responses ----------------------
 
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
@@ -60,29 +132,54 @@ def get_welcome_response():
         card_title, speech_output, reprompt_text, should_end_session))
 
 
-def handle_session_end_request():
-    card_title = "Session Ended"
-    speech_output = "Thanks for playing!"
-    # Setting this to true ends the session and exits the skill.
-    should_end_session = True
-    return build_response({}, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
-
-
-def create_favorite_color_attributes(favorite_color):
-    return {"favoriteColor": favorite_color}
-
-
-def set_color_in_session(intent, session):
-    """ Sets the color in the session and prepares the speech to reply to the
-    user.
+def GameSetup(intent, session):
+    """ Sets up the board based on DifficultyValue.
     """
 
     card_title = intent['name']
     session_attributes = {}
     should_end_session = False
 
-    if 'Color' in intent['slots']:
+
+    # This should always evaluate to true
+    difficulty = intent['slots']['DifficultyValue']['value']
+    if difficulty.lower() == "easy" or difficulty.lower() == "medium":
+        chipsOnBoard == randint(18, 27)
+    else:
+        chipsOnBoard == (4 * randint(5, 8)) + 1 + 2  # Alexa's first move will always be 2
+    speech_output = "Ok, we'll play on " + difficulty + " difficulty. There are " + str(chipsOnBoard) + " chips on the table. I pick up 2 chips. There are now" + str(chipsOnBoard - 2) + " chips on the table.How many chips do you take?"
+    chipsOnBoard = chipsOnBoard - 2
+    session_attributes = {"chipsOnBoard": chipsOnBoard, "difficulty": difficulty}
+    reprompt_text = "Sorry, that sounded like an invalid number of chips. How many chips do you take?"
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def GameLoop(intent, session):
+    """ The core game loop, keeps going until no more chips left on table. Order of this function: user(input already in intent), then Alexa."""
+    card_title = intent['name']
+    should_end_session = False
+
+    chipsOnBoard = session["sessionAttributes"]["chipsOnBoard"]
+    difficulty = session["sessionAttributes"]["difficulty"]
+    chipsOnBoard = chipsOnBoard - intent["slots"]["Number"]["value"]  # subtracts the user's selection
+
+    ### Consider: Win conditions and edge case of the final turn before the win condition.
+
+    if difficulty.lower() == "easy" or difficulty.lower() == "medium":
+        AlexaSelection = randint(1, 3)
+    else:
+        AlexaSelection = 4 - intent["slots"]["Number"]["value"]
+    speech_output = "There are now " + str(chipsOnBoard) + " chips on the table. I'll take " + str(AlexaSelection) + ". Now there's " + str(chipsOnBoard - AlexaSelection) + " chips left. How many do you want to take away?"
+    chipsOnBoard = chipsOnBoard - AlexaSelection
+    reprompt_text = "Sorry, that sounded like an invalid number of chips. There are " + str(chipsOnBoard) + " left on the table. How many do you take?"
+    session_attributes = {"chipsOnBoard": chipsOnBoard, "difficulty": difficulty}
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+
+'''
         favorite_color = intent['slots']['Color']['value']
         session_attributes = create_favorite_color_attributes(favorite_color)
         speech_output = "I now know your favorite color is " + \
@@ -99,8 +196,10 @@ def set_color_in_session(intent, session):
                         "my favorite color is red."
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
+        '''
 
 
+'''
 def get_color_from_session(intent, session):
     session_attributes = {}
     reprompt_text = None
@@ -120,80 +219,18 @@ def get_color_from_session(intent, session):
     # understood, the session will end.
     return build_response(session_attributes, build_speechlet_response(
         intent['name'], speech_output, reprompt_text, should_end_session))
+        '''
+
+def handle_session_end_request():
+    card_title = "Session Ended"
+    speech_output = "Thanks for playing!"
+    # Setting this to true ends the session and exits the skill.
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
 
 
-# --------------- Events ------------------
-
-def on_session_started(session_started_request, session):
-    """ Called when the session starts """
-
-    print("on_session_started requestId=" + session_started_request['requestId']
-          + ", sessionId=" + session['sessionId'])
-
-
-def on_launch(launch_request, session):
-    """ Called when the user launches the skill without specifying what they
-    want
-    """
-
-    print("on_launch requestId=" + launch_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
-    # Dispatch to your skill's launch
-    return get_welcome_response()
-
-
-def on_intent(intent_request, session):
-    """ Called when the user specifies an intent for this skill """
-
-    print("on_intent requestId=" + intent_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
-
-    intent = intent_request['intent']
-    intent_name = intent_request['intent']['name']
-
-    # Dispatch to your skill's intent handlers
-    if intent_name == "MyColorIsIntent":
-        return set_color_in_session(intent, session)
-    elif intent_name == "WhatsMyColorIntent":
-        return get_color_from_session(intent, session)
-    elif intent_name == "AMAZON.HelpIntent":
-        return get_welcome_response()
-    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
-        return handle_session_end_request()
-    else:
-        raise ValueError("Invalid intent")
-
-
-def on_session_ended(session_ended_request, session):
-    """ Called when the user ends the session.
-
-    Is not called when the skill returns should_end_session=true
-    """
-    print("on_session_ended requestId=" + session_ended_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
-    # add cleanup logic here
-
-
-# --------------- Main handler ------------------
-
-def lambda_handler(event, context):
-    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
-    etc.) The JSON body of the request is provided in the event parameter.
-    """
-    print("event.session.application.applicationId=" +
-          event['session']['application']['applicationId'])
-
-    if (event['session']['application']['applicationId'] !=
-             "amzn1.echo-sdk-ams.app.26e18db1-50b1-46e9-86d1-eb9df427e0bf"):
-        raise ValueError("Invalid Application ID")
-
-    if event['session']['new']:
-        on_session_started({'requestId': event['request']['requestId']},
-                           event['session'])
-
-    if event['request']['type'] == "LaunchRequest":
-        return on_launch(event['request'], event['session'])
-    elif event['request']['type'] == "IntentRequest":
-        return on_intent(event['request'], event['session'])
-    elif event['request']['type'] == "SessionEndedRequest":
-        return on_session_ended(event['request'], event['session'])
+'''
+def create_favorite_color_attributes(favorite_color):
+    return {"favoriteColor": favorite_color}
+'''
